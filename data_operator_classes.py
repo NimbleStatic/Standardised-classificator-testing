b82c36927e954fbb6f8d2d97897343b87be98a49
Gustaw <01178232@pw.edu.pl>
2024-02-03 22:47:36 +0100

    added initial Data converter class

---
+++ data_operator_classes.py
@@ -0,0 +1,15 @@
+import numpy as np
+from typing import Callable, List, Dict, Tuple
+from numpy import array
+
+
+class ClassificationDataConverter:
+    def __init__(self):
+        self.conversion_dict_to_key = {}
+        self.conversion_range = (None, None)
+
+    def _create_reverse_dict(self, dict_to_reverse):
+        pass
+
+    def convert_data_classes_to_int(self, Y: List):
+        pass
