2494fa8b95838723376a4a2d0698b32022f76ed9
Gustaw <01178232@pw.edu.pl>
2024-02-04 20:53:07 +0100

    Added data operation classes

--- data_operator_classes.py
+++ data_operator_classes.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Callable, List, Dict, Tuple
 from numpy import array
+import random
 
 
 class ClassificationDataConverter:
@@ -13,3 +14,80 @@ class ClassificationDataConverter:
 
     def convert_data_classes_to_int(self, Y: List):
         pass
+
+
+class BaseDataOperator:
+    def __init__(self):
+        pass
+
+    def select_data_from_Y(
+        self, X: List[any], Y: List[any], classes_to_retain: List[any]
+    ):
+        new_x = []
+        new_y = []
+        for i in range(len(Y)):
+            if Y[i] in classes_to_retain:
+                new_x.append(X[i])
+                new_y.append(Y[i])
+        return new_x, new_y
+
+    def binarise_data(self, Y: List[float], binarisation_level: float):
+        new_y = []
+        for y in Y:
+            if y >= binarisation_level:
+                new_y.append(1)
+            else:
+                new_y.append(-1)
+        return new_y
+
+    def choose_data_randomly_per_class(
+        self, nr_to_get_per_class: Dict[any, int], X: List[any], Y: List[any]
+    ):
+        new_y = []
+        new_x = []
+        index_list = [*range(len(Y))]
+        random.shuffle(index_list)
+        for i in index_list:
+            nr_to_get = nr_to_get_per_class[Y[i]]
+            if nr_to_get > 0:
+                new_y.append(Y[i])
+                new_x.append(X[i])
+                nr_to_get_per_class[Y[i]] = nr_to_get - 1
+        return new_x, new_y
+
+    def get_possible_classes(self, Y: List[any]) -> List:
+        classes = []
+        for y in Y:
+            if y not in classes:
+                classes.append(y)
+        return classes
+
+    def get_nr_of_samples_per_class(self, Y: List[any]) -> Dict[any, int]:
+        count_per_class = {}
+        for y in Y:
+            if y in count_per_class:
+                count_per_class[y] += 1
+            else:
+                count_per_class[y] = 1
+        count_per_class = dict(sorted(count_per_class.items()))
+        return count_per_class
+
+
+class PermanentDataOperations:
+    def __init__(self, X, Y):
+        # The class instance is specific to the data inputed in the init, dont use with different data sets
+        self.X = X
+        self.Y = Y
+        self._calculate_normalisation_parameter_limits()
+
+    def _calculate_normalisation_parameter_limits(self):
+        self.min_limit = self.X[0]
+        self.max_limit = self.X[0]
+
+        for x in self.X[1:]:
+            for i, value in enumerate(x):
+                self.min_limit[i] = min(self.min_limit[i], value)
+                self.max_limit[i] = max(self.max_limit[i], value)
+
+    def normalise_sample(self, x: array):
+        x_capped = np.clip(x, self.min_limit, self.max_limit)
