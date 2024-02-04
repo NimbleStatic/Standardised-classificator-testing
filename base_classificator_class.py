7e8765787e97597463d56f6823e57cce8eb05973
Gustaw <01178232@pw.edu.pl>
2024-02-04 10:31:56 +0100

    added training requirements and parameters

--- base_classificator_class.py
+++ base_classificator_class.py
@@ -6,10 +6,33 @@ from random import choice
 import timeit
 
 
+class TrainingParams:
+    def __init__(
+        self,
+        max_iterations: int = None,
+        epsilon: float = None,
+        optimiser: BaseOptimiser = None,
+    ):
+        self.max_iter = max_iterations
+        self.eps = epsilon
+        self.opt = optimiser
+
+
+class TrainingRequirements:
+    def __init__(self):
+        self.max_iter = False
+        self.eps = False
+        self.opt = False
+
+
 class BaseClassificator:
     def __init__(self) -> None:
         self.classes = []
         self.training_stats = []
+
+    def get_training_requirements(self) -> TrainingRequirements:
+        tr = TrainingRequirements()
+        return tr
 
     def classify_sample(self, x: array):
         return choice(self.classes)
@@ -17,9 +40,7 @@ class BaseClassificator:
     def classify_sample_list(self, X: List[array]) -> List[int]:
         return [self.classify_sample(x) for x in X]
 
-    def train_on_data(
-        self, X: List[array], Y: List[int], optimiser: BaseOptimiser = None
-    ):
+    def train_on_data(self, X: List[array], Y: List[int], tp: TrainingParams) -> dict:
         training_note = {}
         t1 = timeit.default_timer()
         for y in Y:
